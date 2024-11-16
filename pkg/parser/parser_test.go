b30a176c303a7522cfdb419239c7fe430d3c2031
TranslationLookasideBuffer <tlb@nullable.org>
2024-11-15 20:13:32 -0700

    Add more script header parser tests.

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -37,6 +37,181 @@ func TestHeader(t *testing.T) {
 				},
 			},
 		},
+		{
+			name:  "extends",
+			input: "ScriptName Foo Extends Bar",
+			want: &ast.Script{
+				Name: &ast.Identifier{
+					Text: "foo",
+					SourceRange: source.Range{
+						ByteOffset: 11,
+						Length:     3,
+						Line:       1,
+						Column:     12,
+					},
+				},
+				Extends: &ast.Identifier{
+					Text: "bar",
+					SourceRange: source.Range{
+						ByteOffset: 23,
+						Length:     3,
+						Line:       1,
+						Column:     24,
+					},
+				},
+				SourceRange: source.Range{
+					ByteOffset: 0,
+					Length:     26,
+					Line:       1,
+					Column:     1,
+				},
+			},
+		},
+		{
+			name:  "hidden",
+			input: "ScriptName Foo Hidden",
+			want: &ast.Script{
+				Name: &ast.Identifier{
+					Text: "foo",
+					SourceRange: source.Range{
+						ByteOffset: 11,
+						Length:     3,
+						Line:       1,
+						Column:     12,
+					},
+				},
+				IsHidden: true,
+				SourceRange: source.Range{
+					ByteOffset: 0,
+					Length:     21,
+					Line:       1,
+					Column:     1,
+				},
+			},
+		},
+		{
+			name:  "conditional",
+			input: "ScriptName Foo Conditional",
+			want: &ast.Script{
+				Name: &ast.Identifier{
+					Text: "foo",
+					SourceRange: source.Range{
+						ByteOffset: 11,
+						Length:     3,
+						Line:       1,
+						Column:     12,
+					},
+				},
+				IsConditional: true,
+				SourceRange: source.Range{
+					ByteOffset: 0,
+					Length:     26,
+					Line:       1,
+					Column:     1,
+				},
+			},
+		},
+		{
+			name:  "hidden_conditional",
+			input: "ScriptName Foo Hidden Conditional",
+			want: &ast.Script{
+				Name: &ast.Identifier{
+					Text: "foo",
+					SourceRange: source.Range{
+						ByteOffset: 11,
+						Length:     3,
+						Line:       1,
+						Column:     12,
+					},
+				},
+				IsHidden:      true,
+				IsConditional: true,
+				SourceRange: source.Range{
+					ByteOffset: 0,
+					Length:     33,
+					Line:       1,
+					Column:     1,
+				},
+			},
+		},
+		{
+			name:  "conditional_hidden",
+			input: "ScriptName Foo Conditional Hidden",
+			want: &ast.Script{
+				Name: &ast.Identifier{
+					Text: "foo",
+					SourceRange: source.Range{
+						ByteOffset: 11,
+						Length:     3,
+						Line:       1,
+						Column:     12,
+					},
+				},
+				IsHidden:      true,
+				IsConditional: true,
+				SourceRange: source.Range{
+					ByteOffset: 0,
+					Length:     33,
+					Line:       1,
+					Column:     1,
+				},
+			},
+		},
+		{
+			name:  "many_flags",
+			input: "ScriptName Foo Conditional Hidden Conditional Hidden",
+			want: &ast.Script{
+				Name: &ast.Identifier{
+					Text: "foo",
+					SourceRange: source.Range{
+						ByteOffset: 11,
+						Length:     3,
+						Line:       1,
+						Column:     12,
+					},
+				},
+				IsHidden:      true,
+				IsConditional: true,
+				SourceRange: source.Range{
+					ByteOffset: 0,
+					Length:     52,
+					Line:       1,
+					Column:     1,
+				},
+			},
+		},
+		{
+			name:  "extends_many_flags",
+			input: "ScriptName Foo Extends Bar Hidden Conditional Hidden Conditional",
+			want: &ast.Script{
+				Name: &ast.Identifier{
+					Text: "foo",
+					SourceRange: source.Range{
+						ByteOffset: 11,
+						Length:     3,
+						Line:       1,
+						Column:     12,
+					},
+				},
+				Extends: &ast.Identifier{
+					Text: "bar",
+					SourceRange: source.Range{
+						ByteOffset: 23,
+						Length:     3,
+						Line:       1,
+						Column:     24,
+					},
+				},
+				IsHidden:      true,
+				IsConditional: true,
+				SourceRange: source.Range{
+					ByteOffset: 0,
+					Length:     64,
+					Line:       1,
+					Column:     1,
+				},
+			},
+		},
 	}
 
 	for _, test := range tests {
