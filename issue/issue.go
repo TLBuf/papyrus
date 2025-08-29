16d2b608b479bd509c407d01efccf3b6383740f7
TranslationLookasideBuffer <tlb@nullable.org>
2025-08-28 20:51:34 -0600

    Update Issues to use keys rather than URIs.

--- issue/issue.go
+++ issue/issue.go
@@ -2,6 +2,9 @@
 package issue
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/TLBuf/papyrus/source"
 )
 
@@ -28,15 +31,26 @@ const (
 	Info
 )
 
+func (s Severity) String() string {
+	switch s {
+	case Error:
+		return "Error"
+	case Warning:
+		return "Warning"
+	case Info:
+		return "Info"
+	default:
+		return fmt.Sprintf("Unknown(%d)", s)
+	}
+}
+
 // Issue describes an issue found while processing input.
 //
 // Issues never represent internal errors encountered in processing, those are
 // conveyed via normal [error] returns.
 type Issue struct {
-	// URI is the URI for this issue.
-	URI URI
-	// Serverity describes how serious the issue is.
-	Severity Severity
+	// Key identifies the overall kind of issue.
+	Key Key
 	// File is the source file where the issue was found.
 	File *source.File
 	// Location is the source location of the issue.
@@ -47,10 +61,76 @@ type Issue struct {
 	Related []Related
 }
 
+// NewIssue returns a new issue at a specific location with a formatted message.
+func NewIssue(key Key, file *source.File, loc source.Location, msg string, args ...any) *Issue {
+	return &Issue{
+		Key:      key,
+		File:     file,
+		Location: loc,
+		Message:  fmt.Sprintf(msg, args...),
+	}
+}
+
+// AppendRelated adds a related source location to this issue.
+func (i *Issue) AppendRelated(file *source.File, loc *source.Location, msg string, args ...any) {
+	i.Related = append(i.Related, Related{
+		File:     file,
+		Location: *loc,
+		Message:  fmt.Sprintf(msg, args...),
+	})
+}
+
+func (i *Issue) String() string {
+	base := fmt.Sprintf(
+		"%s %s:%d:%d - %s\n",
+		i.Key,
+		i.File.Path(),
+		i.File.StartLine(i.Location),
+		i.File.StartColumn(i.Location),
+		i.Message,
+	)
+	if len(i.Related) == 0 {
+		return base
+	}
+	var sb strings.Builder
+	_, _ = sb.WriteString(base)
+	for _, r := range i.Related {
+		_, _ = sb.WriteString(r.String())
+		_, _ = sb.WriteRune('\n')
+	}
+	return sb.String()
+}
+
+// Key is a unique identifier of a particular type of issue.
+type Key struct {
+	// Serverity describes how serious the issue is.
+	Severity Severity
+	// Namespace defines the namespace of the issue, usually a system identifier.
+	Namespace string
+	// ID is the unqiue identifier of the issue within the namespace.
+	ID string
+}
+
+func (k Key) String() string {
+	return fmt.Sprintf("%s [%s:%s]", k.Severity, k.Namespace, k.ID)
+}
+
 // Related is a separate piece of source code that relates to some issue.
 type Related struct {
+	// File is the source file where the related piece of information is found.
+	File *source.File
 	// Location is the source location of a related piece of information.
 	Location source.Location
 	// Messate is a human-readable description of how this location relates.
 	Message string
+}
+
+func (r Related) String() string {
+	return fmt.Sprintf(
+		"%s:%d:%d - %s",
+		r.File.Path(),
+		r.File.StartLine(r.Location),
+		r.File.StartColumn(r.Location),
+		r.Message,
+	)
 }
--- issue/log.go
+++ issue/log.go
@@ -6,7 +6,7 @@ import (
 
 // Log is an ordered record of issues collected during processing.
 type Log struct {
-	issues   []Issue
+	issues   []*Issue
 	errors   int
 	warnings int
 	infos    int
@@ -18,8 +18,8 @@ func NewLog() *Log {
 }
 
 // Append appends an issue to the log.
-func (l *Log) Append(issue Issue) {
-	switch issue.Severity {
+func (l *Log) Append(issue *Issue) {
+	switch issue.Key.Severity {
 	case Error:
 		l.errors++
 	case Warning:
@@ -41,8 +41,8 @@ func (l *Log) HasErrors() bool {
 }
 
 // All returns an iterator over all issues in the log.
-func (l *Log) All() iter.Seq2[int, Issue] {
-	return func(yield func(int, Issue) bool) {
+func (l *Log) All() iter.Seq2[int, *Issue] {
+	return func(yield func(int, *Issue) bool) {
 		for i, u := range l.issues {
 			if !yield(i, u) {
 				return
@@ -52,33 +52,33 @@ func (l *Log) All() iter.Seq2[int, Issue] {
 }
 
 // Errors returns an iterator over all [Error] issues in the log.
-func (l *Log) Errors() iter.Seq[Issue] {
+func (l *Log) Errors() iter.Seq[*Issue] {
 	if l.errors == 0 {
-		return func(func(Issue) bool) {}
+		return func(func(*Issue) bool) {}
 	}
 	return l.iter(Error)
 }
 
 // Warnings returns an iterator over all [Warning] issues in the log.
-func (l *Log) Warnings() iter.Seq[Issue] {
+func (l *Log) Warnings() iter.Seq[*Issue] {
 	if l.warnings == 0 {
-		return func(func(Issue) bool) {}
+		return func(func(*Issue) bool) {}
 	}
 	return l.iter(Warning)
 }
 
 // Infos returns an iterator over all [Info] issues in the log.
-func (l *Log) Infos() iter.Seq[Issue] {
+func (l *Log) Infos() iter.Seq[*Issue] {
 	if l.infos == 0 {
-		return func(func(Issue) bool) {}
+		return func(func(*Issue) bool) {}
 	}
 	return l.iter(Info)
 }
 
-func (l *Log) iter(severity Severity) iter.Seq[Issue] {
-	return func(yield func(Issue) bool) {
+func (l *Log) iter(severity Severity) iter.Seq[*Issue] {
+	return func(yield func(*Issue) bool) {
 		for _, i := range l.issues {
-			if i.Severity == severity && !yield(i) {
+			if i.Key.Severity == severity && !yield(i) {
 				return
 			}
 		}
